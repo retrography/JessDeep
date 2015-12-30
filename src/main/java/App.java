bcf3ead245f34120271fff5ce6ee7dc03bf8b804
Mahmood Shafeie Zargar <mahmood@gmail.com>
2015-12-29 19:19:29 -0500

    sync

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 /**
  * Created by Mah on 2015-4-24.
  */
@@ -6,55 +8,23 @@ public class App {
 
     public static void main(String[] args) {
 
-/*
+
         Scanner scan = new Scanner(System.in);
-        String s1 = scan.next();
-        String s2 = scan.next();
-*/
+        System.out.print("Signatures: ");
+        String s = scan.next();
+        String[] ss = s.split(",");
 
 
-        String s1 = "96:XaYjV6257bsanRv3U0UgFKeUdPTU/aTeUp906KF76b/UAUwCz6jFt6U6H4UPFYFW:Xa2kanyim/6uOQSOV8Q6uNzu6Z6";
-        String s2 = "96:xaYjV6257bsanRv3U0UgFKeUdPTU/aTeUp906KF76b/UAUwCz6jFt6U6H4UPFYFI:xa2kanyim/6uOQSOV8Q6uNhu6Z6";
+        String s1 = ss[0];
+        String s2 = ss[1];
 
-        SpamSumSignature sss1 = new SpamSumSignature(s1);
-        SpamSumSignature sss2 = new SpamSumSignature(s2);
 
-        Ssdeep ssd = new Ssdeep();
-        int score = ssd.Compare(sss1, sss2);
+        int score = SpamSumSignature.fuzzyCompare(s1, s2);
 
         System.out.println(score);
 
-/*
-
-        RUN_TEST("", "Hello world", 12, "Empty source");
-        RUN_TEST("Hello world", "", 12, "Empty destination");
-        RUN_TEST("Hello world", "Hello world", 0, "Equal strings");
-        RUN_TEST("Hello world", "Hell world", 1, "Delete");
-        RUN_TEST("Hell world", "Hello world", 1, "Insert");
-        RUN_TEST("Hello world", "Hello owrld", 2, "Swap");
-        RUN_TEST("Hello world", "HellX world", 2, "Change");
-
-
-*/
 
     }
 
-    public static void RUN_TEST(String s3, String s4, int out, String msg) {
 
-//        int dist1 = EditDistance.edit_distn(s3.getBytes(), s3.length(), s4.getBytes(), s4.length());
-        int dist2 = EditDistance.edit_distance(s3, s4);
-
-        System.out.println(msg);
-
-//        System.out.print("Old: ");
-//        System.out.println(dist1 == out);
-//        System.out.print("New: ");
-        System.out.print(dist2 == out);
-        System.out.print(": ");
-        System.out.print(out);
-        System.out.print(" vs ");
-        System.out.println(dist2);
-
-
-    }
 }
--- src/main/java/SpamSumSignature.java
+++ src/main/java/SpamSumSignature.java
@@ -1,4 +1,3 @@
-
 /*
  * Copyright (C) 2013 Marius Mailat http://fastlink2.com/contact.htm
  *
@@ -15,78 +14,284 @@
  * limitations under the License.
  */
 
+/*
+ * Modified levenshtein distance calculation
+ * <p/>
+ * This program can be used, redistributed or modified under any of
+ * Boost Software License 1.0, GPL v2 or GPL v3
+ * See the file COPYING for details.
+ * <p/>
+ * $Id$
+ * <p/>
+ * Copyright (C) 2014 kikairoya <kikairoya@gmail.com>
+ * Copyright (C) 2014 Jesse Kornblum <research@jessekornblum.com>
+ */
+
+import java.nio.ByteBuffer;
 import java.util.Arrays;
 
 public class SpamSumSignature {
+
+    public static final int SPAMSUM_LENGTH = 64;
+    public static final int REMOVE_COST = 1;
+    public static final int INSERT_COST = 1;
+    public static final int REPLACE_COST = 2;
+    public static final int MIN_BLOCKSIZE = 3;
+    public static final int ROLLING_WINDOW = 7;
+
     private long blockSize;
     private byte[] hash1;
     private byte[] hash2;
+    private boolean deDup;
 
-    /**
-     * SpamSumSignature
-     * <p/>
-     * Initializes a new instance of the {@code SpamSumSignature} class.
-     *
-     * @param signature - the signature
-     */
-    public SpamSumSignature(String signature) {
-        if (null == signature)
-            throw new IllegalArgumentException("Signature string cannot be null or empty." + "\r\nParameter name: " + "signature");
-
-        int idx1 = signature.indexOf(':');
-        int idx2 = signature.indexOf(':', idx1 + 1);
-
-        if (idx1 < 0)
-            throw new IllegalArgumentException("Signature is not valid." + "\r\nParameter name: " + "signature");
-
-        if (idx2 < 0)
-            throw new IllegalArgumentException("Signature is not valid." + "\r\nParameter name: " + "signature");
-
-        blockSize = Integer.parseInt(signature.substring(0, idx1));
-        hash1 = GetBytes(signature.substring(idx1 + 1, idx1 + 1 + idx2 - idx1 - 1));
-        hash2 = GetBytes(signature.substring(idx2 + 1));
-    }
-
-    /**
-     * SpamSumSignature
-     *
-     * @param blockSize r
-     * @param hash1     r
-     * @param hash2     r
-     */
-    public SpamSumSignature(long blockSize, byte[] hash1, byte[] hash2) {
+    public SpamSumSignature(long blockSize, byte[] hash1, byte[] hash2, boolean deDup) {
         this.blockSize = blockSize;
-        this.hash1 = hash1;
-        this.hash2 = hash2;
+        this.deDup = deDup;
+        this.hash1 = deDup ? eliminateSequences(hash1) : hash1;
+        this.hash2 = deDup ? eliminateSequences(hash2) : hash2;
     }
 
-    /**
-     * GetBytes
-     * <p/>
-     * Change a String into an array of bytes
-     *
-     * @param str s
-     * @return - byte representation of the string
-     */
-    public static byte[] GetBytes(String str) {
-        byte[] r = new byte[str.length()];
-        for (int i = 0; i < r.length; i++)
-            r[i] = (byte) str.charAt(i);
-        return r;
+    public SpamSumSignature(long blockSize, byte[] hash1, byte[] hash2) {
+        this(blockSize, hash1, hash2, false);
     }
 
-    /**
-     * GetString
-     * <p/>
-     * Change an array of bytes into a String
-     *
-     * @param hsh s
-     * @return w
-     */
-    public static String GetString(byte[] hsh) {
-        String r = "";
-        for (byte aHsh : hsh) r += (char) aHsh;
-        return r;
+    public SpamSumSignature(String[] signature, boolean deDup) {
+        this(Long.parseUnsignedLong(signature[0]), signature[1].getBytes(), signature[2].getBytes(), deDup);
+    }
+
+    public SpamSumSignature(String[] signature) {
+        this(signature, false);
+    }
+
+    public SpamSumSignature(String signature, boolean deDup) {
+        this(signature.split(":"), deDup);
+    }
+
+    public SpamSumSignature(String signature) {
+        this(signature, false);
+    }
+
+    private static class RollingState {
+        public int[] windows;
+        public long h1, h2, h3;
+        public long n;
+
+        public RollingState() {
+            windows = new int[ROLLING_WINDOW];
+            h1 = h2 = h3 = n = 0;
+        }
+
+        public long rollHash(int c) {
+
+            this.h2 = this.h2 - this.h1;
+            this.h2 = this.h2 + (ROLLING_WINDOW * c);
+
+            this.h1 = this.h1 + c;
+            this.h1 = this.h1 - this.windows[(int) (Long.remainderUnsigned(this.n, ROLLING_WINDOW))];
+            this.windows[(int) (Long.remainderUnsigned(this.n, ROLLING_WINDOW))] = c;
+            this.n++;
+            if (this.n == ROLLING_WINDOW)
+                this.n = 0;
+            // The original spamsum AND'ed this value with 0xFFFFFFFF which
+            // in theory should have no effect. This AND has been removed for performance
+            this.h3 = this.h3 << 5;
+            this.h3 = this.h3 ^ c;
+            //this.h3 = (this.h3 ^ c) & 0xffffffffL;
+
+            return this.h1 + this.h2 + this.h3;
+        }
+
+    }
+
+    private static boolean hasCommonSubstring(byte[] s1, byte[] s2) {
+        int i, j;
+        int numHashes;
+        long[] hashes = new long[SPAMSUM_LENGTH];
+
+        // there are many possible algorithms for common substring
+        // detection. In this case I am re-using the rolling hash code
+        // to act as a filter for possible substring matches
+        RollingState rollState = new RollingState();
+
+        // first compute the windowed rolling hash at each offset in the first string
+        for (i = 0; i < s1.length; i++) {
+            hashes[i] = rollState.rollHash(s1[i]);
+        }
+        numHashes = i;
+
+        rollState = new RollingState();
+
+        // now for each offset in the second string compute the rolling hash and compare it to all of the rolling hashes
+        // for the first string. If one matches then we have a
+        // candidate substring match. We then confirm that match with a direct string comparison
+
+        for (i = 0; i < s2.length; i++) {
+            long h = rollState.rollHash(s2[i]);
+            if (i < ROLLING_WINDOW - 1) continue;
+            for (j = ROLLING_WINDOW - 1; j < numHashes; j++) {
+                if (hashes[j] == h)
+                {
+                    if (s2.length >= i + 1) {
+
+                        byte[] tmp2 = Arrays.copyOfRange(s2, i - ROLLING_WINDOW + 1, i + 1);
+                        byte[] tmp1 = Arrays.copyOfRange(s1, j - ROLLING_WINDOW + 1, j + 1);
+                        if (Arrays.equals(tmp1, tmp2)) return true;
+                    }
+
+                }
+            }
+        }
+
+        return false;
+    }
+
+
+    private static int editDistance(byte[] s1, byte[] s2) {
+
+        int[][] t = new int[2][SPAMSUM_LENGTH+1];
+        int[] temp;
+
+        int i1, i2;
+        for (i2 = 0; i2 <= s2.length; i2++)
+            t[0][i2] = i2 * REMOVE_COST;
+        for (i1 = 0; i1 < s1.length; i1++) {
+            t[1][0] = (i1 + 1) * INSERT_COST;
+            for (i2 = 0; i2 < s2.length; i2++) {
+                int cost_a = t[0][i2+1] + INSERT_COST;
+                int cost_d = t[1][i2] + REMOVE_COST;
+                int cost_r = t[0][i2] + (s1[i1] == s2[i2] ? 0 : REPLACE_COST);
+                t[1][i2+1] = Math.min(Math.min(cost_a, cost_d), cost_r);
+            }
+            temp = t[0];
+            t[0] = t[1];
+            t[1] = temp;
+        }
+        return t[0][s2.length];
+    }
+
+
+    private static int scoreStrings(byte[] s1, byte[] s2, long block_size) {
+        int score;
+
+        // not a real spamsum signature?
+        if (s1.length > SPAMSUM_LENGTH || s2.length > SPAMSUM_LENGTH) return 0;
+
+        // the two strings must have a common substring of length ROLLING_WINDOW to be candidates
+        if (!hasCommonSubstring(s1, s2)) return 0;
+
+        // compute the edit distance between the two strings. The edit distance gives
+        // us a pretty good idea of how closely related the two strings are
+        score = editDistance(s1, s2);
+
+        // scale the edit distance by the lengths of the two
+        // strings. This changes the score to be a measure of the
+        // proportion of the message that has changed rather than an
+        // absolute quantity. It also copes with the variability of the string lengths
+        score = (score * SPAMSUM_LENGTH) / (s1.length + s2.length);
+
+        // at this stage the score occurs roughly on a 0-64 scale,
+        // with 0 being a good match and 64 being a complete mismatch
+
+        // rescale to a 0-100 scale (friendlier to humans)
+        score = (100 * score) / SPAMSUM_LENGTH;
+
+        // now re-scale on a 0-100 scale with 0 being a poor match and 100 being a excellent match
+        score = 100 - score;
+
+        // when the blocksize is small we don't want to exaggerate the match size
+        if (block_size >= (99 + ROLLING_WINDOW) / ROLLING_WINDOW * MIN_BLOCKSIZE)
+            return score;
+
+        if (score > Long.divideUnsigned(block_size, MIN_BLOCKSIZE) * Math.min(s1.length, s2.length)) {
+            score = (int)Long.divideUnsigned(block_size, MIN_BLOCKSIZE) * Math.min(s1.length, s2.length);
+        }
+        return score;
+    }
+
+
+
+    public boolean isComparable(SpamSumSignature signature) {
+        long blockSize1 = signature.getBlockSize();
+        long blockSize2 = this.getBlockSize();
+
+        if (blockSize1 != blockSize2 &&
+                (blockSize1 > Long.MAX_VALUE / 2 || blockSize1 * 2 != blockSize2) &&
+                (Long.remainderUnsigned(blockSize1, 2) == 1  || Long.divideUnsigned(blockSize1, 2) != blockSize2)) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    public int compareTo(SpamSumSignature other) {
+
+        if (null == other) {
+            return -1;
+        }
+
+        // if the blocksizes don't match then we are comparing
+        // apples to oranges. This isn't an 'error' per se. We could
+        // have two valid signatures, but they can't be compared.
+        if (!this.isComparable(other))
+            return 0;
+
+        // Now that we know the strings are both well formed, are they
+        // identical? We could save ourselves some work here
+        if (this.isPerfectMatch(other))
+            return 100;
+
+        // each spamsum is prefixed by its block size
+        long blockSize1 = this.getBlockSize();
+        long blockSize2 = other.getBlockSize();
+
+        // there is very little information content is sequences of
+        // the same character like 'LLLLL'. Eliminate any sequences
+        // longer than 3. This is especially important when combined
+        // with the hasCommonSubstring() test below.
+        byte[] h11 = this.getHash1(true);
+        byte[] h21 = other.getHash1(true);
+        byte[] h12 = this.getHash2(true);
+        byte[] h22 = other.getHash2(true);
+
+        // each signature has a string for two block sizes. We now
+        // choose how to combine the two block sizes. We checked above
+        // that they have at least one block size in common
+        if (blockSize1 <= Long.MAX_VALUE / 2) {
+            if (blockSize1 == blockSize2) {
+                int score1, score2;
+                score1 = scoreStrings(h11, h21, blockSize1);
+                score2 = scoreStrings(h12, h22, blockSize1 * 2);
+                return Math.max(score1, score2);
+            } else if (blockSize1 * 2 == blockSize2) {
+                return scoreStrings(h12, h21, blockSize2);
+            } else {
+                return scoreStrings(h11, h22, blockSize1);
+            }
+        } else {
+            if (blockSize1 == blockSize2) {
+                return scoreStrings(h11, h21, blockSize1);
+            } else if (Long.remainderUnsigned(blockSize1, 2) == 1  || Long.divideUnsigned(blockSize1, 2) != blockSize2) {
+                return scoreStrings(h11, h22, blockSize1);
+            } else {
+                return 0;
+            }
+        }
+
+    }
+
+    public static int fuzzyCompare(String signature1, String signature2) {
+
+        if (signature1.length() < 1 || signature2.length() <1) {
+            return -1;
+        }
+        SpamSumSignature sig1 = new SpamSumSignature(signature1);
+        SpamSumSignature sig2 = new SpamSumSignature(signature2);
+
+        return sig1.compareTo(sig2);
+    }
+
+    public String getSignature() {
+        return Long.toUnsignedString(blockSize) + ':' + Arrays.toString(hash1) + ':' + Arrays.toString(hash2);
     }
 
     public boolean equals(Object obj) {
@@ -95,46 +300,78 @@ public class SpamSumSignature {
     }
 
     public boolean equals(SpamSumSignature other) {
+        return this.isDeDupped() == other.isDeDupped() &&
+                this.getBlockSize() == other.getBlockSize() &&
+                Arrays.equals(this.getHash1(), other.getHash1()) &&
+                Arrays.equals(this.getHash2(), other.getHash2());
+    }
+
+
+    public boolean isPerfectMatch(SpamSumSignature other) {
         return this.blockSize == other.blockSize && (Arrays.equals(this.hash1, other.hash1) || Arrays.equals(this.hash2, other.hash2));
 
     }
 
-    public String toString() {
-        String hashText1 = GetString(hash1);
-        String hashText2 = GetString(hash2);
-        return blockSize + ":" + hashText1 + ":" + hashText2;
+    public void deDup() {
+        if (!deDup) {
+            hash1 = eliminateSequences(hash1);
+            hash2 = eliminateSequences(hash2);
+            this.deDup = true;
+        }
     }
 
-    /**
-     * getBlockSize
-     * <p/>
-     * Gets the size of the block.
-     *
-     * @return - the size of the block.
-     */
+    public boolean isDeDupped() {
+        return this.deDup;
+    }
+
+    public byte[] toByteArray() {
+        byte[] blockSize = Long.toUnsignedString(this.blockSize).getBytes();
+        return ByteBuffer.allocate(hash1.length + hash2.length + blockSize.length + 2)
+                .put(blockSize)
+                .put((byte) ':')
+                .put(hash1)
+                .put((byte)':')
+                .put(hash2)
+                .array();
+    }
+
+    public String toString() {
+        return getSignature();
+    }
+
     public long getBlockSize() {
         return blockSize;
     }
 
-    /**
-     * getHashPart1
-     * <p/>
-     * Gets the first hash part.
-     *
-     * @return - the first hash part.
-     */
-    public byte[] getHashPart1() {
-        return hash1;
+    public long[] getBlockSizes() {
+        return new long[] {blockSize, blockSize * 2};
     }
 
-    /**
-     * getHashPart2
-     * <p/>
-     * Gets the second hash part.
-     *
-     * @return - the second hash part.
-     */
-    public byte[] getHashPart2() {
-        return hash2;
+    private byte[] eliminateSequences(byte[] str) {
+        int i, j, len;
+        len = str.length;
+        byte[] ret = new byte[len];
+
+        for (i = j = 3; i < len; i++) {
+            if (str[i] != str[i - 1] || str[i] != str[i - 2] || str[i] != str[i - 3]) {
+                ret[j++] = str[i];
+            }
+        }
+
+        return ret;
     }
+
+    public byte[] getHash1(boolean deDup) {
+        return ((deDup && !this.isDeDupped()) ? eliminateSequences(hash1) : hash1);
+    }
+
+    public byte[] getHash1() { return getHash1(false); }
+
+
+    public byte[] getHash2(boolean deDup) {
+        return ((deDup && !this.isDeDupped()) ? eliminateSequences(hash2) : hash2);
+    }
+
+    public byte[] getHash2() { return getHash2(false); }
+
 }
